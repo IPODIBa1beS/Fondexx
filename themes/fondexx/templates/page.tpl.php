33689a942482e3c59067a251520b4dd24752b6f3
ArialBlack <andreyturik@gmail.com>
2016-09-15 17:14:09 +0300

    add styles

--- themes/fondexx/templates/page.tpl.php
+++ themes/fondexx/templates/page.tpl.php
@@ -75,34 +75,45 @@
 ?>
 
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="<?php print $container_class; ?>">
+    <div class="<?php print $container_class; ?>">
+    
     <div class="navbar-header">
-      <?php if ($logo): ?>
-        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
-
-      <?php if (!empty($site_name)): ?>
-        <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
-
-      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
+            <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
         </button>
-      <?php endif; ?>
-    </div>
+          
+        <?php if ($logo): ?>
+            <a class="logo navbar-btn" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+                <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+            </a>
+        <?php endif; ?>
+        
+        <?php if (!empty($primary_nav)): ?>
+            <?php print render($primary_nav); ?>
+        <?php endif; ?>
 
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
+    </div>
+    
+    <hr>
+    
+    <!-- second level menu clone -->
+    <div class="second-level-menu">
+        <?php if (!empty($primary_nav)): ?>
+                <?php print render($primary_nav); ?>
+         <?php endif; ?>
+     </div>
+
+    <!-- Overlay menu -->
+    <div class="navbar-collapse collapse">
         <nav role="navigation">
+          
           <?php if (!empty($primary_nav)): ?>
             <?php print render($primary_nav); ?>
           <?php endif; ?>
+          
           <?php if (!empty($secondary_nav)): ?>
             <?php print render($secondary_nav); ?>
           <?php endif; ?>
@@ -110,11 +121,12 @@
             <?php print render($page['navigation']); ?>
           <?php endif; ?>
         </nav>
-      </div>
-    <?php endif; ?>
+    </div>
+    
   </div>
-  00
 </header>
+
+<hr>
 
 <div class="main-container <?php print $container_class; ?>">
 
