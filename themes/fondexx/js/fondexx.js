99aac525a3b6f5bafa1302bf0e7f94bad9a868a3
ArialBlack <andreyturik@gmail.com>
2016-09-20 17:52:47 +0300

    add wrapTabs

--- themes/fondexx/js/fondexx.js
+++ themes/fondexx/js/fondexx.js
@@ -18,6 +18,34 @@
             });  
         }
         
+        function wrapTabs(container, child, header, content) {
+            var $conatainer = $(container),
+                $child = $conatainer.find(child);
+                
+            
+            $(container).prepend('<div class="tab-content"></div>');
+            $(container).prepend('<ul class="nav nav-tabs" role="tablist"></ul>');
+            
+            $child.each(function( index ) {
+                var $this = $(this);
+                    $header = $this.find(header),
+                    $content = $this.find(content);
+                    activeclass = '';
+                    
+                    if (index === 0) {
+                        activeclass = 'active';
+                    } else {
+                        activeclass = '';
+                    }
+                    
+                    $header.wrap('<li role="presentation" class="nav-li ' + activeclass + '"><a href="#tab' + index + '" aria-controls="tab' + index + '" role="tab" data-toggle="tab"></a></li>');
+                    $(container).find('li.nav-li').appendTo($(container).find('ul.nav-tabs'));
+                    
+                    $content.wrap('<div role="tabpanel" class="tab-pane ' + activeclass + '" id="tab' + index +'"></div>');
+                    $(container).find('.tab-pane').appendTo($(container).find('.tab-content'));
+            });        
+        };
+        
         function replaceTablePlusMinus2icons() {
             $("table td").filter(function() {
                 return $(this).text() === "+";
@@ -36,7 +64,8 @@
             // let's disable Bootstrap dropdown beh. for primary nav. We don't need it at all
             //disableBootstrapMenuDropdown();
             
-            wrapCollapse('.panels-flexible-row-3-main-row', '.panel-pane', 'h2', '.pane-content');
+            wrapCollapse('#panels-ipe-display-panel-context-page-fondexx-start-page-fondexx-start-panel-context-2b4e6245-be03-4570-83d0-741f48f90afc .panels-flexible-row-3-main-row', '.panel-pane', 'h2', '.pane-content');
+            wrapTabs('#panels-ipe-display-panel-context-page-home-page-home-panel-context-e1182d54-3e51-409b-b6a0-4e0719a62c9c .panels-flexible-row-7-1', '.panel-pane', 'h2', '.pane-content');
             replaceTablePlusMinus2icons();
         });
 
