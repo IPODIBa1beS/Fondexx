40185795d88263f84a8902b142c029d8cc96ff0b
vratyshnyi <ratyshniyv@gmail.com>
2016-10-14 18:24:35 +0300

    Hotfix styles;

---
+++ themes/fondexx/js/custom.js
@@ -0,0 +1,13 @@
+(function() {
+    $(document).ready(function () {
+
+        $('.close-overlay').on('click', function () {
+            alert('hi');
+            $(".navbar-collapse").removeClass('in');
+        });
+
+        $('.navbar-nav .dropdown-toggle').on('click', function () {
+            alert('hello');
+        })
+    });
+})();
