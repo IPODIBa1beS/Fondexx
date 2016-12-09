327825203df5c15630d82a0e4f75a8750e3bb156
ArialBlack <andreyturik@gmail.com>
2016-12-09 12:30:55 +0200

    tel changes

--- themes/fondexx/dev-templates/parts/header.php
+++ themes/fondexx/dev-templates/parts/header.php
@@ -39,7 +39,7 @@
             <a href="/study/faq" class="button btn">faq</a>
             </div>
             <ul class="header_contacts-list">
-                <li><a href="/company/contacts" class="header_contacts-link phone">+38 (044)-200-55-21</a></li>
+                <li><a href="/company/contacts" class="header_contacts-link phone">+38 (044)-332-00-18</a></li>
                 <li><a href="/company/contacts" class="header_contacts-link phone">+38 (044)-200-55-21</a></li>
                 <li><a href="#" class="header_contacts-link skype">live:fondexx_1</a></li>
             </ul>
