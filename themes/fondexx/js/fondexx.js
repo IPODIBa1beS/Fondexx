3ee7f0a711797cc5b1c446b2e94ff3bcc571fe95
vratyshnyi <ratyshniyv@gmail.com>
2017-04-07 00:12:51 +0300

    New landings: - basic styles for investing and lightspeed.

--- themes/fondexx/js/fondexx.js
+++ themes/fondexx/js/fondexx.js
@@ -1,56 +1,56 @@
 (function ($) {
     $(function() {
-        
+
         /*function disableBootstrapMenuDropdown() {
             $('#navbar .navbar-nav .dropdown-toggle').data('toggle', '').removeAttr('data-toggle');;
         }*/
-        
+
         function wrapCollapse(container, child, header, content) {
             var $container = $(container),
                 $child = $container.find(child);
-            
+
             $child.each(function( index ) {
                 var $this = $(this);
                     $header = $this.find(header),
                     $content = $this.find(content),
                     $header.wrap( '<a class="" role="button" data-toggle="collapse" href="#collapse' + index + '" aria-expanded="false" aria-controls="collapse' + index + '"></a>');
                     $content.wrap('<div class="collapse" id="collapse' + index + '"></div>');
-            });  
+            });
         }
-        
+
         function wrapTabs(container, child, header, content) {
             var $conatainer = $(container),
                 $child = $conatainer.find(child);
-                
-            
+
+
             $(container).prepend('<div class="tab-content"></div>');
             $(container).prepend('<ul class="nav nav-tabs" role="tablist"></ul>');
-            
+
             $child.each(function( index ) {
                 var $this = $(this);
                     $header = $this.find(header),
                     $content = $this.find(content);
                     activeclass = '';
-                    
+
                     if (index === 0) {
                         activeclass = 'active';
                     } else {
                         activeclass = '';
                     }
-                    
+
                     $header.wrap('<li role="presentation" class="nav-li ' + activeclass + '"><a href="#tab' + index + '" aria-controls="tab' + index + '" role="tab" data-toggle="tab"></a></li>');
                     $(container).find('li.nav-li').appendTo($(container).find('ul.nav-tabs'));
-                    
+
                     $content.wrap('<div role="tabpanel" class="tab-pane ' + activeclass + '" id="tab' + index +'"></div>');
                     $(container).find('.tab-pane').appendTo($(container).find('.tab-content'));
-            });        
+            });
         };
-        
+
         function replaceTablePlusMinus2icons() {
             $("table td").filter(function() {
                 return $(this).text() === "+";
             }).html("<i class='ion-ios-checkmark-empty'></i>");
-            
+
             $("table td").filter(function() {
                 return $(this).text() === "-";
             }).html("<i class='ion-ios-close-empty'></i>");
@@ -181,10 +181,10 @@
         /////////////////////////////////////
         /////////////////////////////////////
         /////////////////////////////////////
-        $( document ).ready(function() {  
+        $( document ).ready(function() {
             // let's disable Bootstrap dropdown beh. for primary nav. We don't need it at all
             //disableBootstrapMenuDropdown();
-            
+
             wrapCollapse('#fondexx-start-landing .panels-flexible-row-3-main-row', '.panel-pane', 'h2', '.pane-content');
             wrapCollapse('.panels-flexible-5 .panels-flexible-row-5-main-row', '.panel-pane', 'h2', '.pane-content');
             wrapTabs('#front-panel .panels-flexible-row-7-1', '.panel-pane', 'h2', '.pane-content');
@@ -432,6 +432,7 @@
             setTimeout(function(){
                 $('.panels-flexible-row-3-1').attr('data-scroll-index', '1');
                 $('.pane-webform-client-block-180').attr('data-scroll-index', '1');
+                $('#webform-client-block-180').attr('data-scroll-index', '1');
             }, 300);
         });
     });
